9d34b603db354cf87eff10b5633d0e8158d35bca
Hong Kiet <36097702+hongkiet@users.noreply.github.com>
2023-05-03 01:45:07 +0700

    DATABASE ACCOUNT AND SANPHAM

--- se_final_project.sql
+++ se_final_project.sql
@@ -25,7 +25,6 @@ CREATE TABLE `sanpham` (
   `tenSP` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `nsx` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `dongia` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `ngaynhap` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `ngayxuat` datetime NOT NULL DEFAULT '9999-12-31 23:59:59',
   `mota` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -34,8 +33,8 @@ CREATE TABLE `sanpham` (
 
 
  
-INSERT INTO `sanpham` (`maSP`, `tenSP`, `nsx`, `dongia`, `email`, `ngaynhap`, `ngayxuat`, `mota`) VALUES ('SP001', 'Samsung Galaxy S21 Ultra 5G', 'Samsung', '25000000', 'samsung@gmail.com', '2022-01-01 00:00:00', '2022-01-07 00:00:00', 'Màn hình 6.8 inch, RAM 12GB, bộ nhớ 256GB, camera 108MP'),
-('SP002', 'iPhone 13 Pro Max', 'Apple', '29990000', 'apple@gmail.com', '2022-02-01 00:00:00', '2022-02-07 00:00:00', 'Màn hình 6.7 inch, RAM 8GB, bộ nhớ 128GB, camera 12MP'),
-('SP003', 'OnePlus 10 Pro', 'OnePlus', '19990000', 'oneplus@gmail.com', '2022-03-01 00:00:00', '2022-03-07 00:00:00', 'Màn hình 6.7 inch, RAM 12GB, bộ nhớ 512GB, camera 64MP'),
-('SP004', 'Xiaomi 12 Ultra', 'Xiaomi', '17990000', 'xiaomi@gmail.com', '2022-04-01 00:00:00', '2022-04-07 00:00:00', 'Màn hình 6.81 inch, RAM 12GB, bộ nhớ 256GB, camera 200MP'),
-('SP005', 'Oppo Find X5 Pro', 'Oppo', '21990000', 'oppo@gmail.com', '2022-05-01 00:00:00', '2022-05-07 00:00:00', 'Màn hình 6.7 inch, RAM 8GB, bộ nhớ 128GB, camera 108MP')
+INSERT INTO `sanpham` (`maSP`, `tenSP`, `nsx`, `dongia`,  `ngaynhap`, `ngayxuat`, `mota`) VALUES ('SP001', 'Samsung Galaxy S21 Ultra 5G', 'Samsung', '25000000', '2022-01-01 00:00:00', '2022-01-07 00:00:00', 'Màn hình 6.8 inch, RAM 12GB, bộ nhớ 256GB, camera 108MP'),
+('SP002', 'iPhone 13 Pro Max', 'Apple', '29990000',  '2022-02-01 00:00:00', '2022-02-07 00:00:00', 'Màn hình 6.7 inch, RAM 8GB, bộ nhớ 128GB, camera 12MP'),
+('SP003', 'OnePlus 10 Pro', 'OnePlus', '19990000',  '2022-03-01 00:00:00', '2022-03-07 00:00:00', 'Màn hình 6.7 inch, RAM 12GB, bộ nhớ 512GB, camera 64MP'),
+('SP004', 'Xiaomi 12 Ultra', 'Xiaomi', '17990000',  '2022-04-01 00:00:00', '2022-04-07 00:00:00', 'Màn hình 6.81 inch, RAM 12GB, bộ nhớ 256GB, camera 200MP'),
+('SP005', 'Oppo Find X5 Pro', 'Oppo', '21990000',  '2022-05-01 00:00:00', '2022-05-07 00:00:00', 'Màn hình 6.7 inch, RAM 8GB, bộ nhớ 128GB, camera 108MP')
